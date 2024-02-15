fd4a6e45e584008f357f98efd4f544c9032c59e0
Carlos Bracho <brachocarlos152@gmail.com>
2024-02-14 21:01:12 -0500

    Changed the text and styling of the buttons on the index page.

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const noButton = document.querySelector("#no-button");
+const yesButton = document.querySelector("#yes-button");
 
-function changeTextNo() {
+function changeText() {
   noButton.innerHTML = "INCORRECT ANSWER";
-  noButton.style.backgroundColor = "#FF3B30";
+  noButton.style.backgroundColor = "red";
+  noButton.style.color = "white";
+  noButton.style.border = "none";
+
+  yesButton.innerHTML = '"ONLY" CORRECT ANSWER';
+  yesButton.style.backgroundColor = "green";
+  yesButton.style.color = "white";
+  yesButton.style.border = "none";
+  yesButton.style.padding = "20px";
 }
 
-noButton.addEventListener("click", changeTextNo);
+noButton.addEventListener("click", changeText);
